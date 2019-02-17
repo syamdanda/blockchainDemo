8fa137b512e67347d9413d62c1741b940397c49a
syamdanda <syam2011@gmail.com>
2019-02-17 19:16:55 +0530

    initial commit

---
+++ dev/blockchain.js
@@ -0,0 +1,21 @@
+
+function Blockchain() {
+	this.blockChain = [];
+	this.data = [];
+}
+
+Blockchain.prototype.createBlock = function (previousHash, currentHash, nounce) {
+	const block = {
+		index: this.blockChain.length + 1,
+		timestamp: new Date().getTime(),
+		data: this.data,
+		nounce: nounce,
+		previousHash: previousHash,
+		currentHash: currentHash
+	}
+
+	this.blockChain.push(block);
+	this.data = [];
+
+	return block;
+}
