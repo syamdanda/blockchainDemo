50a668199c7b58ab44b900402fe0e5b76e8fe8b8
syamdanda <syam2011@gmail.com>
2019-02-18 00:38:31 +0530

    added server.js

---
+++ dev/server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+	res.json('Hello World');
+});
+
+
+
+
+
+
+
+
+// set app defaults
+var app_host = process.env.HOST || 'localhost';
+var app_port = process.env.PORT || 9009;
+
+// lift the app
+app.listen(app_port, app_host, function (){
+    console.log('Seatcode - A blockchain app listening on host: http://' + app_host + ':' + app_port);    
+}).on('error', function (err){
+    if(err.code === 'EADDRINUSE'){
+        console.error('Error starting app: Port ' + app_port + ' already in use, choose another');
+    }else{
+        console.error('Error starting app: ' + err);
+        app.emit('error');
+    }
+});
