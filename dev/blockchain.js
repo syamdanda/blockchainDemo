7e54d6e2fb167062e69a6d19f71478fc0a60153e
syamdanda <syam2011@gmail.com>
2019-02-17 20:04:21 +0530

    converted blockchain to seatcode

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,35 +1,38 @@
 
 const _ = require('underscore');
 
-function Blockchain() {
-	this.blockChain = [];
-	this.records = [];
+function SeatCodeChain() {
+	this.seatCodes = [];
+	this.seatCodeInfo = {
+		'empId': 0,
+		'seatCode': null
+	};
 }
 
-Blockchain.prototype.createBlock = function (previousHash, currentHash, nounce) {
-	const block = {
-		index: this.blockChain.length + 1,
-		timestamp: new Date().getTime(),
-		records: this.records,
-		nounce: nounce,
-		previousHash: previousHash,
-		hash: currentHash
+SeatCodeChain.prototype.createBlock = function (previousHash, currentHash, nounce) {
+	const seatCodeObj = {
+		'index': this.seatCodes.length + 1,
+		'timestamp': new Date().getTime(),
+		'seatCodeInfo': this.seatCodeInfo,
+		'nounce': nounce,
+		'previousHash': previousHash,
+		'hash': currentHash
 	}
-	this.blockChain.push(block);
-	this.records = [];
-	return block;
+	this.seatCodes.push(seatCodeObj);
+	this.seatCodeInfo = {};
+	return seatCodeObj;
 }
 
-Blockchain.prototype.getBlock = function(hash) {
-	return _.find(this.blockChain, function(block) { 
-		return block.hash == hash; 
+SeatCodeChain.prototype.getSeat = function(hash) {
+	return _.find(this.seatCodes, function(seatCodeObj) { 
+		return seatCodeObj.hash == hash; 
 	});
 }
 
-Blockchain.prototype.getOriginBlock = function() {
-	return this.blockChain[0];
+SeatCodeChain.prototype.getOriginBlock = function() {
+	return this.seatCodes[0];
 }
 
-Blockchain.prototype.getLastBlock = function() {
-	return this.blockChain[this.blockChain.length - 1];
+SeatCodeChain.prototype.getLastBlock = function() {
+	return this.seatCodes[this.SeatCodeChain.length - 1];
 }
