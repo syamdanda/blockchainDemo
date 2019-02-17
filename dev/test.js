38c96fb41e2dd3591d7dd07b85beac8f530c3995
syamdanda <syam2011@gmail.com>
2019-02-17 20:45:36 +0530

    changes made in test.js

--- dev/test.js
+++ dev/test.js
@@ -2,8 +2,10 @@ const SeatCodeChain = require('./seatCodeChain');
 
 const seatCode = new SeatCodeChain();
 
-seatCode.createSeatCode('A0DFGHJYU', 'A1RTYUJBVD', 123);
+seatCode.createSeatCode('A0', 'A1', 123);
 
 seatCode.requestSeatCode('C1', 'EMP1', 'SEAT1');
 
-console.log(seatCode);
+seatCode.createSeatCode('A1', 'A2', 321);
+
+console.log(seatCode.seatCodes[1]);
