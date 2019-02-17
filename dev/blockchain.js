cff56b8a7540ae80cd9e38ea31e559cdaf07b9a6
syamdanda <syam2011@gmail.com>
2019-02-17 19:49:45 +0530

    modified block structure

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -3,33 +3,33 @@ const _ = require('underscore');
 
 function Blockchain() {
 	this.blockChain = [];
-	this.data = [];
+	this.records = [];
 }
 
 Blockchain.prototype.createBlock = function (previousHash, currentHash, nounce) {
 	const block = {
 		index: this.blockChain.length + 1,
 		timestamp: new Date().getTime(),
-		data: this.data,
+		records: this.records,
 		nounce: nounce,
 		previousHash: previousHash,
 		hash: currentHash
 	}
 	this.blockChain.push(block);
-	this.data = [];
+	this.records = [];
 	return block;
 }
 
 Blockchain.prototype.getBlock = function(hash) {
-	return _.find(this.data, function(block) { 
+	return _.find(this.blockChain, function(block) { 
 		return block.hash == hash; 
 	});
 }
 
 Blockchain.prototype.getOriginBlock = function() {
-	return this.data[0];
+	return this.blockChain[0];
 }
 
 Blockchain.prototype.getLastBlock = function() {
-	return this.data[this.data.length - 1];
+	return this.blockChain[this.blockChain.length - 1];
 }
